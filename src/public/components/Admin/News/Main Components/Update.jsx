d2d68cb9175e34d5b93070e028633d6a18cd5ada
Constanda Valentin Marian <constandavalentin@gmail.com>
2017-07-27 13:24:30 +0300

    Update of previous commit

--- src/public/components/Admin/News/Main Components/Update.jsx
+++ src/public/components/Admin/News/Main Components/Update.jsx
@@ -275,7 +275,7 @@ class Update extends Component {
                                     label={stepIndex === 1 ? "Save" : "Next"}
                                     primary={true}
                                     onTouchTap={stepIndex === 1 ? this.props.onSave : this.handleNext}
-                                    buttonStyle={{backgroundColor: "#9b9b9b"}}/>
+                                    buttonStyle={stepIndex === 2 ? {backgroundColor: "green"} : {backgroundColor: "#9b9b9b"}}/>
                             </CardActions>
                         </Card>
                     </Card>
