1626179f0feae55575d2d31a0bfae84d9e817cb8
Constanda Valentin Marian <constandavalentin@gmail.com>
2017-09-04 23:57:11 +0300

    4Art

--- resource/authentication/login.js
+++ resource/authentication/login.js
@@ -66,7 +66,7 @@ module.exports = new PassportLocalStrategy(
                 };
 
                 // create the token
-                const token = jwt.sign(payload, config.jwtSecret);
+                const token = jwt.sign(payload, config.jwtSecret, { expiresIn: 60 * 60 * 2 });
 
                 return done(null, token);
             });
